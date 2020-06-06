32c9005332eedf0e44c22a1ac5295044e3307de1
GtxFury <32810165+GtxFury@users.noreply.github.com>
2020-06-06 10:49:26 +0800

    Add files via upload

---
+++ check.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 最大的CPU占用率
+maxCPUPercuentage=90
+# 日志输出路径
+logFilePath="temp.log"
+
+containersInfo=`docker stats -a --no-stream --format '{{.Container}}:{{.CPUPerc}}'`
+currentDate=`date +"%Y-%m-%d %H:%M:%S"`
+
+echo "${containersInfo}" | while read line
+do
+    currentContainerId=`echo ${line} | cut -d ':' -f 1`
+    currentContainerCPUPercuentage=`echo ${line} | cut -d ':' -f 2`
+    currentContainerNeedRestart=$(echo "${currentContainerCPUPercuentage:0:-1} > ${maxCPUPercuentage}" | bc)
+    echo "${currentDate} ${currentContainerId} 的CPU占用率是: ${currentContainerCPUPercuentage}"
+    if [ ${currentContainerNeedRestart} = 1 ]
+    then
+        echo "${currentDate} 正在重启容器: ${currentContainerId}" >> ${logFilePath}
+        docker restart ${currentContainerId}
+    fi
+done
---
+++ dockerclean.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+logs=$(find /var/lib/docker/containers/ -name *-json.log*)
+for log in $logs
+do
+	echo "clean logs : $log"
+	cat /dev/null > $log
+done
+echo "end"
