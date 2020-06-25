0fddb5067508c1474f7cabde72708bcc2ed452e1
GtxFury <32810165+GtxFury@users.noreply.github.com>
2020-06-25 16:38:08 +0800

    Update checkname.sh

--- checkname.sh
+++ checkname.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 最大的CPU占用率
-maxCPUPercuentage=90
+maxCPUPercuentage=1
 # 日志输出路径
 logFilePath="temp.log"
 
-containersInfo=docker stats --no-stream --format '{{.Container}}:{{.CPUPerc}}' $*
-currentDate=date +"%Y-%m-%d %H:%M:%S"
+containersInfo=`docker stats --no-stream --format '{{.Container}}:{{.CPUPerc}}' $*`
+currentDate=`date +"%Y-%m-%d %H:%M:%S"`
 
 echo "${containersInfo}" | while read line
 do
-    currentContainerId=echo ${line} | cut -d ':' -f 1
-    currentContainerCPUPercuentage=echo ${line} | cut -d ':' -f 2
+    currentContainerId=`echo ${line} | cut -d ':' -f 1`
+    currentContainerCPUPercuentage=`echo ${line} | cut -d ':' -f 2`
     currentContainerNeedRestart=$(echo "${currentContainerCPUPercuentage:0:-1} > ${maxCPUPercuentage}" | bc)
     echo "${currentDate} ${currentContainerId} 的CPU占用率是: ${currentContainerCPUPercuentage}"
     if [ ${currentContainerNeedRestart} = 1 ]
